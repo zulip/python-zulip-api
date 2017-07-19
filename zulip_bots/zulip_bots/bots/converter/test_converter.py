13352889043db9a3afa852247fe389e52396a56e
Abhijeet Kaur <abhijeetkaur96@gmail.com>
2017-07-20 02:26:41 +0530

    bots: Add test fixture file for converter bot.

--- zulip_bots/zulip_bots/bots/converter/test_converter.py
+++ zulip_bots/zulip_bots/bots/converter/test_converter.py
@@ -9,14 +9,4 @@ class TestConverterBot(BotTestCase):
     bot_name = "converter"
 
     def test_bot(self):
-        expected = {
-            "": ('Too few arguments given. Enter `@convert help` '
-                 'for help on using the converter.\n'),
-            "foo bar": ('Too few arguments given. Enter `@convert help` '
-                        'for help on using the converter.\n'),
-            "2 m cm": "2.0 m = 200.0 cm\n",
-            "12.0 celsius fahrenheit": "12.0 celsius = 53.600054 fahrenheit\n",
-            "0.002 kilometer millimile": "0.002 kilometer = 1.2427424 millimile\n",
-            "3 megabyte kilobit": "3.0 megabyte = 24576.0 kilobit\n",
-        }
-        self.check_expected_responses(expected)
+        self.check_expected_responses(test_file_name='test_1')
