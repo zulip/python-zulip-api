cc009a2838837a0f4ed5e3be2ca4cca6d40e594f
Abhijeet Kaur <abhijeetkaur96@gmail.com>
2017-07-20 02:45:25 +0530

    bots: Add test fixture file for define bot.

--- zulip_bots/zulip_bots/bots/define/test_define.py
+++ zulip_bots/zulip_bots/bots/define/test_define.py
@@ -9,12 +9,4 @@ class TestDefineBot(BotTestCase):
     bot_name = "define"
 
     def test_bot(self):
-        expected = {
-            "": 'Please enter a word to define.',
-            "foo": "**foo**:\nDefinition not available.",
-            "cat": ("**cat**:\n\n* (**noun**) a small domesticated carnivorous mammal "
-                    "with soft fur, a short snout, and retractile claws. It is widely "
-                    "kept as a pet or for catching mice, and many breeds have been "
-                    "developed.\n&nbsp;&nbsp;their pet cat\n\n"),
-        }
-        self.check_expected_responses(expected)
+        self.check_expected_responses(test_file_name='test_1')
