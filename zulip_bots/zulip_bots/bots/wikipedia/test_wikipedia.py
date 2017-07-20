da6d751e392e5fe71b5afa4fd7516d08f7efcd9e
Abhijeet Kaur <abhijeetkaur96@gmail.com>
2017-07-20 16:32:40 +0530

    bots: Add test fixture file for wikipedia bot.

--- zulip_bots/zulip_bots/bots/wikipedia/test_wikipedia.py
+++ zulip_bots/zulip_bots/bots/wikipedia/test_wikipedia.py
@@ -9,15 +9,4 @@ class TestWikipediaBot(BotTestCase):
     bot_name = "wikipedia"
 
     def test_bot(self):
-        expected = {
-            "": 'Please enter your message after @mention-bot',
-            "sssssss kkkkk": ('I am sorry. The search term you provided '
-                              'is not found :slightly_frowning_face:'),
-            "foo": ('For search term "foo", '
-                    'https://en.wikipedia.org/wiki/Foobar'),
-            "123": ('For search term "123", '
-                    'https://en.wikipedia.org/wiki/123'),
-            "laugh": ('For search term "laugh", '
-                      'https://en.wikipedia.org/wiki/Laughter'),
-        }
-        self.check_expected_responses(expected)
+        self.check_expected_responses(test_file_name='test_1')
