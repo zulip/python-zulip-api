35353f6d7f93f36c9cfbc053978873eaddc1db57
Abhijeet Kaur <abhijeetkaur96@gmail.com>
2017-07-23 02:22:09 +0530

    bots: Add complete tests for offline testing of wikipedia bot.

--- zulip_bots/zulip_bots/bots/wikipedia/test_wikipedia.py
+++ zulip_bots/zulip_bots/bots/wikipedia/test_wikipedia.py
@@ -9,15 +9,47 @@ class TestWikipediaBot(BotTestCase):
     bot_name = "wikipedia"
 
     def test_bot(self):
-        expected = {
-            "": 'Please enter your message after @mention-bot',
-            "sssssss kkkkk": ('I am sorry. The search term you provided '
-                              'is not found :slightly_frowning_face:'),
-            "foo": ('For search term "foo", '
-                    'https://en.wikipedia.org/wiki/Foobar'),
-            "123": ('For search term "123", '
-                    'https://en.wikipedia.org/wiki/123'),
-            "laugh": ('For search term "laugh", '
-                      'https://en.wikipedia.org/wiki/Laughter'),
-        }
-        self.check_expected_responses(expected)
+
+        # Single-word query
+        bot_response = "For search term \"happy\", https://en.wikipedia.org/wiki/Happiness"
+        with self.mock_http_conversation('test_single_word'):
+            self.assert_bot_response(
+                message = {'content': 'happy'},
+                response = {'content': bot_response},
+                expected_method='send_reply'
+            )
+
+        # Multi-word query
+        bot_response = "For search term \"The sky is blue\", https://en.wikipedia.org/wiki/Sky_blue"
+        with self.mock_http_conversation('test_multi_word'):
+            self.assert_bot_response(
+                message = {'content': 'The sky is blue'},
+                response = {'content': bot_response},
+                expected_method='send_reply'
+            )
+
+        # Number query
+        bot_response = "For search term \"123\", https://en.wikipedia.org/wiki/123"
+        with self.mock_http_conversation('test_number_query'):
+            self.assert_bot_response(
+                message = {'content': '123'},
+                response = {'content': bot_response},
+                expected_method='send_reply'
+            )
+
+        # Incorrect word
+        bot_response = "I am sorry. The search term you provided is not found :slightly_frowning_face:"
+        with self.mock_http_conversation('test_incorrect_query'):
+            self.assert_bot_response(
+                message = {'content': 'sssssss kkkkk'},
+                response = {'content': bot_response},
+                expected_method='send_reply'
+            )
+
+        # Empty query, no request made to the Internet.
+        bot_response = "Please enter your message after @mention-bot"
+        self.assert_bot_response(
+            message = {'content': ''},
+            response = {'content': bot_response},
+            expected_method='send_reply'
+        )
