372717f4f8fa6b1d3b3c7d1900ff7bf233017b24
Rhea Parekh <rheaparekh12@gmail.com>
2017-10-08 14:49:13 +0530

    slack: Add zerver_usermessage object.

--- zulip/integrations/slack/slackdata2zulipdata.py
+++ zulip/integrations/slack/slackdata2zulipdata.py
@@ -131,7 +131,6 @@ def channels2zerver_stream(slack_dir, realm_id, added_users):
     zerver_recipient = []
     subscription_id_count = 1
     zerver_defaultstream = {}
-    
 
     for channel in channels:
         # slack_channel_id = channel['id']
@@ -303,7 +302,7 @@ def main(slack_zip_file: str) -> None:
     realm = dict(zerver_client=[{"name": "populate_db", "id": 1},
                                 {"name": "website", "id": 2},
                                 {"name": "API", "id": 3}],
-                 zerver_userpresence=[],  # TODO
+                 zerver_userpresence=[],  # shows last logged in data, which is not available in slack
                  zerver_userprofile_mirrordummy=[],
                  zerver_realmdomain=[{"realm": REALM_ID,
                                       "allow_subdomains": False,
@@ -337,11 +336,28 @@ def main(slack_zip_file: str) -> None:
     # now for message.json
     message_json = {}
     zerver_message = []
-    # TODO map zerver_usermessage
+    zerver_usermessage = []
+
     for channel in added_channels.keys():
         zerver_message.append(channelmessage2zerver_message(slack_dir, channel,
                               added_users, added_channels))
+    # construct the usermessage object and append it to zerver_usermessage
+    usermessage_id = 1
+    for zulip_message in zerver_message:
+        for subscription in zerver_subscription:
+            if subscription['recipient'] == zulip_message['recipient']:
+                usermessage = dict(
+                    user_profile=subscription['user_profile'],
+                    id=usermessage_id,
+                    flags_mask=1,  # defaulting to 'read'
+                    message=zulip_message['id'])
+                usermessage_id += 1
+                zerver_usermessage.append(usermessage)
+    # TOODO add zerver_usermessage corresponding to the
+    # private messages and huddles type recipients
+
     message_json['zerver_message'] = zerver_message
+    message_json['zerver_usermessage'] = zerver_usermessage
     # IO
     message_file = output_dir + '/message.json'
     json.dump(message_json, open(message_file, 'w'))
