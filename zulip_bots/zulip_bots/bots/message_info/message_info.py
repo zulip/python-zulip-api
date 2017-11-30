0de677b0388d5a9b080ead0775aa3949c68180b8
Skunk <theskunkmb@gmail.com>
2017-11-29 17:46:53 -0800

    zulip_bots: Set up the basic `message_info` bot.
    Create a description in `usage` and a handler.

---
+++ zulip_bots/zulip_bots/bots/message_info/message_info.py
@@ -0,0 +1,19 @@
+# See readme.md for instructions on running this code.
+
+
+class MessageInfoHandler(object):
+    def usage(self):
+        return '''This bot will allow users to analyze a message for letter
+            count and word count. The gathered information will then be sent to
+            a private conversation with the user. Users should @-mention the
+            bot in the beginning of a message.
+            '''
+
+    def handle_message(self, message, bot_handler):
+        words_in_message = message['content'].split()
+
+        unformatted_content = 'You sent a message with {} words.'
+        content = unformatted_content.format(len(words_in_message))
+
+
+handler_class = HelloWorldHandler
