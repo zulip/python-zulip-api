29e22c2c0ad97caebd452b2a786a1338b6b9f86b
Skunk <theskunkmb@gmail.com>
2017-12-01 20:33:52 -0800

    bots: Create Link Shortener Bot.
    Create Link Shortener Bot using the goo.gl Link Shortening API.
    
    Link Shortener Bot can be mentioned in a conversation, and it will
    respond with shortened, goo.gl links for every URL in the message.
    
    For example,
    
     > @link_shortener_bot @johnsmith Check out this file:
     > https://github.com/zulip/python-zulip-api/blob/master/zulip_bots/
    
    and Link Shortener Bot would respond
    
     > https://github.com/zulip/python-zulip-api/blob/master/zulip_bots/:
     > https://goo.gl/Mt5z3c
    
    In order to use Link Shortener Bot, an API key for goo.gl must be set
    in `link_shortener.conf` in the `link_shortener` folder.

---
+++ zulip_bots/zulip_bots/bots/link_shortener/link_shortener.py
@@ -0,0 +1,80 @@
+import re
+import requests
+
+class LinkShortenerHandler(object):
+    '''A Zulip bot that will shorten URLs ("links") in a conversation using the
+    goo.gl URL shortener.
+    '''
+
+    def usage(self):
+        return (
+            'Mention the link shortener bot in a conversation and then enter '
+            'any URLs you want to shorten in the body of the message. \n\n'
+            '`key` must be set in `link_shortener.conf`.')
+
+    def initialize(self, bot_handler):
+        self.config_info = bot_handler.get_config_info('link_shortener')
+
+    def handle_message(self, message, bot_handler):
+        REGEX_STR = (
+            '('
+            '(?:http|https):\/\/'  # This allows for the HTTP or HTTPS
+                                   # protocol.
+            '[^"<>#%\{\}|\\^~[\]` ]+'  # This allows for any character except
+                                       # for certain non-URL-safe ones.
+            ')'
+        )
+
+        content = message['content']
+
+        if content.strip() == 'help':
+            bot_handler.send_reply(
+                message,
+                (
+                    'Mention the link shortener bot in a conversation and '
+                    'then enter any URLs you want to shorten in the body of '
+                    'the message.'
+                )
+            )
+            return
+
+        link_matches = re.findall(REGEX_STR, content)
+
+        shortened_links = [self.shorten_link(link) for link in link_matches]
+        link_pairs = [
+            (link_match + ': ' + shortened_link)
+            for link_match, shortened_link
+            in zip(link_matches, shortened_links)
+            if shortened_link != ''
+        ]
+        final_response = '\n'.join(link_pairs)
+
+        if final_response == '':
+            bot_handler.send_reply(
+                message,
+                'No links found. Send "help" to see usage instructions.'
+            )
+            return
+
+        bot_handler.send_reply(message, final_response)
+
+    def shorten_link(self, long_url):
+        '''Shortens a link using goo.gl Link Shortener and returns it, or
+        returns an empty string if something goes wrong.
+
+        Parameters:
+            long_url (str): The original URL to shorten.
+        '''
+
+        body = {'longUrl': long_url}
+        params = {'key': self.config_info['key']}
+
+        request = requests.post(
+            'https://www.googleapis.com/urlshortener/v1/url',
+            json=body,
+            params=params
+        )
+
+        return request.json().get('id', '')
+
+handler_class = LinkShortenerHandler
