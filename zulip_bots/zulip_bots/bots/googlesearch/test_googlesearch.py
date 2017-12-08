6f0d5239e8c83dd4eea80534d040c8873176b03b
Steve Howell <showell@zulipchat.com>
2017-12-07 19:28:24 -0800

    test_googlesearch: Remove `self: Any` annotations.
    We can avoid `Any` annotations for `self`, since it is more
    noise than signal and since the type of self is already
    implicit from how Python classes work.

--- zulip_bots/zulip_bots/bots/googlesearch/test_googlesearch.py
+++ zulip_bots/zulip_bots/bots/googlesearch/test_googlesearch.py
@@ -2,20 +2,18 @@
 
 from zulip_bots.test_lib import StubBotTestCase
 
-from typing import Any
-
 class TestGoogleSearchBot(StubBotTestCase):
     bot_name = 'googlesearch'
 
     # Simple query
-    def test_normal(self: Any) -> None:
+    def test_normal(self) -> None:
         with self.mock_http_conversation('test_normal'):
             self.verify_reply(
                 'zulip',
                 'Found Result: [Zulip](https://www.google.com/url?url=https%3A%2F%2Fzulipchat.com%2F)'
             )
 
-    def test_bot_help(self: Any) -> None:
+    def test_bot_help(self) -> None:
         help_message = "To use this bot, start messages with @mentioned-bot, \
                     followed by what you want to search for. If \
                     found, Zulip will return the first search result \
@@ -27,6 +25,6 @@ class TestGoogleSearchBot(StubBotTestCase):
         self.verify_reply('', help_message)
         self.verify_reply('help', help_message)
 
-    def test_bot_no_results(self: Any) -> None:
+    def test_bot_no_results(self) -> None:
         with self.mock_http_conversation('test_no_result'):
             self.verify_reply('no res', 'Found no results.')
