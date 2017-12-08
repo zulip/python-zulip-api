62246cba4a51d3bac60759be85f769c3d322858d
Steve Howell <showell@zulipchat.com>
2017-12-07 17:28:38 -0800

    Remove unneeded imports in test_googlesearch.py.

--- zulip_bots/zulip_bots/bots/googlesearch/test_googlesearch.py
+++ zulip_bots/zulip_bots/bots/googlesearch/test_googlesearch.py
@@ -1,12 +1,5 @@
 #!/usr/bin/env python
 
-from __future__ import absolute_import
-from __future__ import print_function
-
-import random
-
-from six.moves.urllib import error
-
 from zulip_bots.test_lib import BotTestCase
 
 from typing import Any
