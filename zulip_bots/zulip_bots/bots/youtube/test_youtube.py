6f9d010ed35004a5836d22bb97dd72c089b50d89
Sivagiri Visakan <siva.g.visakan@gmail.com>
2017-12-09 15:32:16 +0530

    bots: Refactor Youtube bot.

---
+++ zulip_bots/zulip_bots/bots/youtube/test_youtube.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+
+from __future__ import absolute_import
+
+from unittest.mock import patch
+from requests.exceptions import HTTPError, ConnectionError
+
+from zulip_bots.test_lib import StubBotHandler, StubBotTestCase, get_bot_message_handler
+from typing import Any, Union, Dict
+class TestYoutubeBot(StubBotTestCase):
+    bot_name = "youtube"
+    normal_config  = {'key': '12345678',
+                      'number_of_results': '5',
+                      'video_region': 'US'}  # type: Dict[str,str]
+
+    def test_single(self) -> None:
+        bot_response = 'Here is what I found for `funny cats` : \n'\
+                       'Cats are so funny you will die laughing - ' \
+                       'Funny cat compilation - [Watch now](https://www.youtube.com/watch?v=5dsGWM5XGdg)'
+
+        with self.mock_config_info(self.normal_config), \
+                self.mock_http_conversation('test_single'):
+                    self.verify_reply('funny cats', bot_response)
+
+    def test_invalid_key(self) -> None:
+        bot = get_bot_message_handler(self.bot_name)
+        bot_handler = StubBotHandler()
+
+        with self.mock_config_info({'key': 'somethinginvalid', 'number_of_results': '5', 'video_region': 'US'}), \
+                self.mock_http_conversation('test_invalid_key'), \
+                self.assertRaises(SystemExit) as se:  # type: ignore
+                    bot.initialize(bot_handler)
+
+    def test_multiple(self) -> None:
+        bot = get_bot_message_handler(self.bot_name)
+        bot_handler = StubBotHandler()
+
+        bot_response = 'Here is what I found for `marvel` : ' \
+                       '\n * Marvel Studios\' Avengers: Infinity War Official Trailer - [Watch now](https://www.youtube.com/watch/6ZfuNTqbHE8)' \
+                       '\n * Marvel Studios\' Black Panther - Official Trailer - [Watch now](https://www.youtube.com/watch/xjDjIWPwcPU)' \
+                       '\n * MARVEL RISING BEGINS! | The Next Generation of Marvel Heroes (EXCLUSIVE) - [Watch now](https://www.youtube.com/watch/6HTPCTtkWoA)' \
+                       '\n * Marvel Contest of Champions Taskmaster Spotlight - [Watch now](https://www.youtube.com/watch/-8uqxdcJ9WM)' \
+                       '\n * 5* Crystal Opening! SO LUCKY! - Marvel Contest Of Champions - [Watch now](https://www.youtube.com/watch/l7rrsGKJ_O4)'
+
+        with self.mock_config_info(self.normal_config), \
+                self.mock_http_conversation('test_multiple'):
+                    self.verify_reply('list marvel', bot_response)
+
+    def test_noresult(self) -> None:
+        bot_response = 'Oops ! Sorry I couldn\'t find any video for  `somethingrandomwithnoresult` ' \
+                       ':slightly_frowning_face:'
+
+        with self.mock_config_info(self.normal_config), \
+                self.mock_http_conversation('test_noresult'):
+            self.verify_reply('somethingrandomwithnoresult', bot_response,)
+
+    def test_help(self) -> None:
+        help_content = "*Help for YouTube bot* :robot_face: : \n\n" \
+                       "The bot responds to messages starting with @mention-bot.\n\n" \
+                       "`@mention-bot <search terms>` will return top Youtube video for the given `<search term>`.\n" \
+                       "`@mention-bot top <search terms>` also returns the top Youtube result.\n" \
+                       "`@mention-bot list <search terms>` will return a list Youtube videos for the given <search term>.\n \n" \
+                       "Example:\n" \
+                       " * @mention-bot funny cats\n" \
+                       " * @mention-bot list funny dogs"
+
+        with self.mock_config_info(self.normal_config), \
+                self.mock_http_conversation('test_keyok'):
+                    self.verify_reply('help', help_content)
+                    self.verify_reply('list', help_content)
+                    self.verify_reply('help list', help_content)
+                    self.verify_reply('top', help_content)
+                    self.verify_reply('', help_content)
+
+    def test_connection_error(self) -> None:
+        with self.mock_config_info(self.normal_config), \
+                patch('requests.get', side_effect=ConnectionError()), \
+                patch('logging.exception'):
+            self.verify_reply('Wow !', 'Uh-Oh, couldn\'t process the request '
+                              'right now.\nPlease again later')
---
+++ zulip_bots/zulip_bots/bots/youtube/youtube.py
@@ -0,0 +1,133 @@
+from __future__ import absolute_import
+
+import requests
+import logging
+import sys
+
+from requests.exceptions import HTTPError, ConnectionError
+from typing import Dict, Any, Union, List, Tuple
+
+commands_list = ('list', 'top', 'help')
+
+class YoutubeHandler(object):
+
+    def usage(self) -> str:
+        return '''
+            This plugin will allow users to search
+            for a given search term on Youtube.
+            Use '@mention-bot help' to get more information on the bot usage.
+            '''
+    help_content = "*Help for YouTube bot* :robot_face: : \n\n" \
+                   "The bot responds to messages starting with @mention-bot.\n\n" \
+                   "`@mention-bot <search terms>` will return top Youtube video for the given `<search term>`.\n" \
+                   "`@mention-bot top <search terms>` also returns the top Youtube result.\n" \
+                   "`@mention-bot list <search terms>` will return a list Youtube videos for the given <search term>.\n \n" \
+                   "Example:\n" \
+                   " * @mention-bot funny cats\n" \
+                   " * @mention-bot list funny dogs"
+
+    def initialize(self, bot_handler: Any) -> None:
+        self.config_info = bot_handler.get_config_info('youtube')
+        # Check if API key is valid. If it is not valid, don't run the bot.
+        try:
+            search_youtube('test', self.config_info['key'], self.config_info['video_region'])
+        except HTTPError as e:
+            if (e.response.json()['error']['errors'][0]['reason'] == 'keyInvalid'):
+                logging.error('Invalid key.'
+                              'Follow the instructions in doc.md for setting API key.')
+                sys.exit(1)
+            else:
+                raise
+        except ConnectionError:
+            logging.warning('Bad connection')
+
+    def handle_message(self, message: Dict[str, str], bot_handler: Any) -> None:
+
+        if message['content'] == '' or message['content'] == 'help':
+            bot_handler.send_reply(message, self.help_content)
+        else:
+            cmd, query = get_command_query(message)
+            bot_response = get_bot_response(query,
+                                            cmd,
+                                            self.config_info)
+            logging.info(bot_response.format())
+            bot_handler.send_reply(message, bot_response)
+
+
+def search_youtube(query: str, key: str,
+                   region: str, max_results: int = 1) -> List[List[str]]:
+
+    videos = []
+    params = {
+        'part': 'id,snippet',
+        'maxResults': max_results,
+        'key': key,
+        'q': query,
+        'alt': 'json',
+        'type': 'video',
+        'regionCode': region}  # type: Dict[str, Union[str, int]]
+
+    url = 'https://www.googleapis.com/youtube/v3/search'
+    try:
+        r = requests.get(url, params=params)
+    except ConnectionError as e:  # Usually triggered by bad connection.
+        logging.exception('Bad connection')
+        raise
+    r.raise_for_status()
+    search_response = r.json()
+    # Add each result to the appropriate list, and then display the lists of
+    # matching videos, channels, and playlists.
+    for search_result in search_response.get('items', []):
+        if search_result['id']['kind'] == 'youtube#video':
+            videos.append([search_result['snippet']['title'],
+                           search_result['id']['videoId']])
+    return videos
+
+
+def get_command_query(message: Dict[str, str]) -> Tuple[Union[None, str], str]:
+    blocks = message['content'].lower().split()
+    command = blocks[0]
+    if command in commands_list:
+        query = message['content'][len(command) + 1:].lstrip()
+        return command, query
+    else:
+        return None, message['content']
+
+
+def get_bot_response(query: Union[str, None], command: Union[str, None], config_info: Dict[str, str]) -> str:
+
+    key = config_info['key']
+    max_results = int(config_info['number_of_results'])
+    region = config_info['video_region']
+    reply = 'Here is what I found for `' + query + '` : '
+    video_list = []   # type: List[List[str]]
+    try:
+        if query == '' or query is None:
+            return YoutubeHandler.help_content
+        if command is None or command == 'top':
+            video_list = search_youtube(query, key, region)
+
+        elif command == 'list':
+            video_list = search_youtube(query, key, region, max_results)
+
+        elif command == 'help':
+            return YoutubeHandler.help_content
+
+    except (ConnectionError, HTTPError):
+        return 'Uh-Oh, couldn\'t process the request ' \
+               'right now.\nPlease again later'
+
+    if len(video_list) == 0:
+        return 'Oops ! Sorry I couldn\'t find any video for  `' + query + '` :slightly_frowning_face:'
+    elif len(video_list) == 1:
+        return (reply + '\n%s - [Watch now](https://www.youtube.com/watch?v=%s)' % (video_list[0][0], video_list[0][1])).strip()
+
+    for title, id in video_list:
+        reply = reply + \
+            '\n * %s - [Watch now](https://www.youtube.com/watch/%s)' % (title, id)
+    # Using link https://www.youtube.com/watch/<id> to
+    # prevent showing multiple previews
+    return reply
+
+
+handler_class = YoutubeHandler
