31764754c7d45f8a55d59be95560f90ddf0c11fa
neiljp (Neil Pilgrim) <github@kepier.clara.net>
2017-12-10 18:15:52 -0800

    [WIP] bots: Simplify test_virtual_fs.py.

--- zulip_bots/zulip_bots/bots/virtual_fs/test_virtual_fs.py
+++ zulip_bots/zulip_bots/bots/virtual_fs/test_virtual_fs.py
@@ -1,12 +1,4 @@
-#!/usr/bin/env python
-
-from __future__ import absolute_import
-from __future__ import print_function
-
 from zulip_bots.test_lib import BotTestCase
-from zulip_bots.lib import StateHandler
-
-from typing import Any
 
 class TestVirtualFsBot(BotTestCase):
     bot_name = "virtual_fs"
