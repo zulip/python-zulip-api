1fd4dfc86e03e85f1f2d8c5ce898c913c832f6ad
neiljp (Neil Pilgrim) <github@kepier.clara.net>
2017-12-22 21:41:17 -0800

    bot testing: Amend StubBotHandler.get_config_info to return {}.
    This ensures the return type matches the annotated type, which
    matches the return type of ExternalBotHandler.

--- zulip_bots/zulip_bots/test_lib.py
+++ zulip_bots/zulip_bots/test_lib.py
@@ -54,7 +54,7 @@ class StubBotHandler:
 
     def get_config_info(self, bot_name, optional=False):
         # type: (str, bool) -> Dict[str, Any]
-        return None
+        return {}
 
     def unique_reply(self):
         # type: () -> Dict[str, Any]
