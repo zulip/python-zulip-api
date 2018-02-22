5fda59dde0be727c9ce9f90d56c01d571b06ec61
amanagr <f2016561@pilani.bits-pilani.ac.in>
2018-02-22 11:19:52 +0530

    Susi AI Bot: Create a susi ai bot.
    This bot uses SUSI_SERVER API to get response.

---
+++ zulip_bots/zulip_bots/bots/susi/test_susi.py
@@ -0,0 +1,45 @@
+from zulip_bots.test_lib import (
+    BotTestCase,
+)
+
+from typing import Any
+
+class TestSusiBot(BotTestCase):
+    bot_name = "susi"
+
+    def test_help(self) -> None:
+        bot_response = '''
+    Hi, I am Susi, people generally ask me these questions:
+    ```
+    What is the exchange rate of USD to BTC
+    How to cook biryani
+    draw a card
+    word starting with m and ending with v
+    question me
+    random GIF
+    image of a bird
+    flip a coin
+    let us play
+    who is Albert Einstein
+    search wikipedia for artificial intelligence
+    when is christmas
+    what is hello in french
+    name a popular movie
+    news
+    tell me a joke
+    buy a dress
+    currency of singapore
+    distance between india and singapore
+    tell me latest phone by LG
+    ```
+        '''
+
+        self.verify_reply('', bot_response)
+        self.verify_reply('help', bot_response)
+
+    def test_issue(self) -> None:
+        request = 'hi'
+        bot_response = 'Hello!'
+
+        with self.mock_http_conversation('test_reply'):
+            self.verify_reply(request, bot_response)
