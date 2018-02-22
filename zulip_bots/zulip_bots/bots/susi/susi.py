dd28bf7478ef1e7b08b7b3f2ebffe77a9924e1be
amanagr <f2016561@pilani.bits-pilani.ac.in>
2018-02-22 11:19:52 +0530

    Susi AI Bot: Create a susi ai bot.
    This bot uses SUSI_SERVER API to get response.

---
+++ zulip_bots/zulip_bots/bots/susi/susi.py
@@ -0,0 +1,51 @@
+import requests
+import urllib
+from typing import Dict, Any, Tuple, Union
+
+class SusiHandler(object):
+    '''
+    Susi AI Bot
+    To create and know more of SUSI skills go to `https://skills.susi.ai/`
+    '''
+
+    def usage(self) -> str:
+        return '''
+    Hi, I am Susi, people generally ask me these questions:
+    ```
+    What is the exchange rate of USD to BTC
+    How to cook biryani
+    draw a card
+    word starting with m and ending with v
+    question me
+    random GIF
+    image of a bird
+    flip a coin
+    let us play
+    who is Albert Einstein
+    search wikipedia for artificial intelligence
+    when is christmas
+    what is hello in french
+    name a popular movie
+    news
+    tell me a joke
+    buy a dress
+    currency of singapore
+    distance between india and singapore
+    tell me latest phone by LG
+    ```
+        '''
+
+    def handle_message(self, message: Dict[str, str], bot_handler: Any) -> None:
+        msg = message['content']
+        if msg == 'help' or msg == '':
+            bot_handler.send_reply(message, self.usage())
+            return
+        encoded_msg = urllib.parse.quote_plus(msg.encode('utf8'))
+        reply = requests.get("https://api.susi.ai/susi/chat.json?q=" + encoded_msg)
+        try:
+            answer = reply.json()['answers'][0]['actions'][0]['expression']
+        except Exception:
+            answer = "I don't understand. Can you rephrase?"
+        bot_handler.send_reply(message, answer)
+
+handler_class = SusiHandler
