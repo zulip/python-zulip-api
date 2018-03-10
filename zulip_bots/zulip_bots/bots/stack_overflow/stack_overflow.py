c3b00e17800edfdc24fec1b1279e709b22555dc4
Dhruv Thakker <dthakker1997@gmail.com>
2018-03-10 18:24:11 +0530

    stack_overflow : Create StackOverflow bot.
    (Use stackoverflow APIs to answer queries.)

---
+++ zulip_bots/zulip_bots/bots/stack_overflow/stack_overflow.py
@@ -0,0 +1,77 @@
+import requests
+import logging
+import re
+import urllib
+from zulip_bots.lib import Any
+
+from typing import Optional, Any, Dict
+
+# See readme.md for instructions on running this code.
+
+class StackOverflowHandler(object):
+    '''
+    This plugin facilitates searching Stack Overflow for a
+    specific query and returns the top 3 questions from the
+    search. It looks for messages starting with '@mention-bot'
+
+    In this example, we write all Stack Overflow searches into
+    the same stream that it was called from.
+    '''
+
+    META = {
+        'name': 'StackOverflow',
+        'description': 'Searches Stack Overflow for a query and returns the top 3 articles.',
+    }
+
+    def usage(self) -> str:
+        return '''
+            This plugin will allow users to directly search
+            Stack Overflow for a specific query and get the top 3
+            articles that are returned from the search. Users
+            should preface query with "@mention-bot".
+            @mention-bot <search query>'''
+
+    def handle_message(self, message: Dict[str, str], bot_handler: Any) -> None:
+        bot_response = self.get_bot_stackoverflow_response(message, bot_handler)
+        bot_handler.send_reply(message, bot_response)
+
+    def get_bot_stackoverflow_response(self, message: Dict[str, str], bot_handler: Any) -> Optional[str]:
+        '''This function returns the URLs of the requested topic.'''
+
+        help_text = 'Please enter your query after @mention-bot to search StackOverflow'
+
+        # Checking if the link exists.
+        query = message['content']
+        if query == '' or query == 'help':
+            return help_text
+
+        query_stack_link = ('http://api.stackexchange.com/2.2/search/advanced?'
+                            'order=desc&sort=relevance&site=stackoverflow&title=%s'
+                            % (urllib.parse.quote(query),))
+        try:
+            data = requests.get(query_stack_link)
+
+        except requests.exceptions.RequestException:
+            logging.error('broken link')
+            return 'Uh-Oh ! Sorry ,couldn\'t process the request right now.:slightly_frowning_face:\n' \
+                   'Please try again later.'
+
+        # Checking if the bot accessed the link.
+        if data.status_code != 200:
+            logging.error('Page not found.')
+            return 'Uh-Oh ! Sorry ,couldn\'t process the request right now.:slightly_frowning_face:\n' \
+                   'Please try again later.'
+
+        new_content = 'For search term:' + query + '\n'
+
+        # Checking if there is content for the searched term
+        if len(data.json()['items']) == 0:
+            new_content = 'I am sorry. The search term you provided is not found :slightly_frowning_face:'
+        else:
+            for i in range(min(3, len(data.json()['items']))):
+                search_string = data.json()['items'][i]['title']
+                link = data.json()['items'][i]['link']
+                new_content += str(i+1) + ' : ' + '[' + search_string + ']' + '(' + link + ')\n'
+        return new_content
+
+handler_class = StackOverflowHandler
---
+++ zulip_bots/zulip_bots/bots/stack_overflow/test_stack_overflow.py
@@ -0,0 +1,54 @@
+from zulip_bots.test_lib import BotTestCase
+from zulip_bots.request_test_lib import mock_request_exception
+
+class TestStackoverflowBot(BotTestCase):
+    bot_name = "stack_overflow"
+
+    def test_bot(self) -> None:
+
+        # Single-word query
+        bot_request = 'restful'
+        bot_response = ('''For search term:restful
+1 : [What exactly is RESTful programming?](https://stackoverflow.com/questions/671118/what-exactly-is-restful-programming)
+2 : [RESTful Authentication](https://stackoverflow.com/questions/319530/restful-authentication)
+3 : [RESTful URL design for search](https://stackoverflow.com/questions/319530/restful-authentication)
+''')
+        with self.mock_http_conversation('test_single_word'):
+            self.verify_reply(bot_request, bot_response)
+
+        # Multi-word query
+        bot_request = 'what is flutter'
+        bot_response = ('''For search term:what is flutter
+1 : [What is flutter/dart and what are its benefits over other tools?](https://stackoverflow.com/questions/49023008/what-is-flutter-dart-and-what-are-its-benefits-over-other-tools)
+''')
+        with self.mock_http_conversation('test_multi_word'):
+            self.verify_reply(bot_request, bot_response)
+
+        # Number query
+        bot_request = '113'
+        bot_response = ('''For search term:113
+1 : [INSTALL_FAILED_NO_MATCHING_ABIS res-113](https://stackoverflow.com/questions/47117788/install-failed-no-matching-abis-res-113)
+2 : [com.sun.tools.xjc.reader.Ring.get(Ring.java:113)](https://stackoverflow.com/questions/12848282/com-sun-tools-xjc-reader-ring-getring-java113)
+3 : [no route to host error 113](https://stackoverflow.com/questions/10516222/no-route-to-host-error-113)
+''')
+        with self.mock_http_conversation('test_number_query'):
+            self.verify_reply(bot_request, bot_response)
+
+        # Incorrect word
+        bot_request = 'narendra'
+        bot_response = "I am sorry. The search term you provided is not found :slightly_frowning_face:"
+        with self.mock_http_conversation('test_incorrect_query'):
+            self.verify_reply(bot_request, bot_response)
+
+        # 404 status code
+        bot_request = 'Zulip'
+        bot_response = 'Uh-Oh ! Sorry ,couldn\'t process the request right now.:slightly_frowning_face:\n' \
+                       'Please try again later.'
+
+        with self.mock_http_conversation('test_status_code'):
+            self.verify_reply(bot_request, bot_response)
+
+        # Request Exception
+        bot_request = 'Z'
+        with mock_request_exception():
+            self.verify_reply(bot_request, bot_response)
