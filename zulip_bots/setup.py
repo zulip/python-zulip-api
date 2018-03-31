7e360c244c60ab7c7ddbbbed4ac50b383ff34cd6
Eeshan Garg <jerryguitarist@gmail.com>
2018-03-31 17:19:23 -0230

    pypa: Release version 0.4.2.

--- zulip_bots/setup.py
+++ zulip_bots/setup.py
@@ -10,7 +10,7 @@ import pip
 if False:
     from typing import Any, Dict, Optional
 
-ZULIP_BOTS_VERSION = "0.4.1"
+ZULIP_BOTS_VERSION = "0.4.2"
 IS_PYPA_PACKAGE = False
 
 
--- zulip_botserver/setup.py
+++ zulip_botserver/setup.py
@@ -8,7 +8,7 @@ import sys
 if False:
     from typing import Any, Dict, Optional
 
-ZULIP_BOTSERVER_VERSION = "0.4.1"
+ZULIP_BOTSERVER_VERSION = "0.4.2"
 
 # We should be installable with either setuptools or distutils.
 package_info = dict(
