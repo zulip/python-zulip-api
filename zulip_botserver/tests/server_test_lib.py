2de862cdcd7388c87166b203d22f73ccbfb6ebc0
dkvasov <diman4eg@mail.ru>
2018-05-16 19:26:51 +0300

    zulip_botserver/tests: Reformat input to make them more readable.

--- zulip_botserver/tests/server_test_lib.py
+++ zulip_botserver/tests/server_test_lib.py
@@ -11,14 +11,15 @@ class BotServerTestCase(TestCase):
         server.app.testing = True
         self.app = server.app.test_client()
 
-    def assert_bot_server_response(self,
-                                   available_bots: Optional[List[str]]=None,
-                                   bots_config: Optional[Dict[str, Dict[str, str]]]=None,
-                                   bot_handlers: Optional[Dict[str, Any]]=None,
-                                   payload_url: str="/bots/helloworld",
-                                   message: Optional[Dict[str, Any]]=dict(message={'key': "test message"}),
-                                   check_success: bool=False,
-                                   ) -> None:
+    def assert_bot_server_response(
+        self,
+        available_bots: Optional[List[str]]=None,
+        bots_config: Optional[Dict[str, Dict[str, str]]]=None,
+        bot_handlers: Optional[Dict[str, Any]]=None,
+        payload_url: str="/bots/helloworld",
+        message: Optional[Dict[str, Any]]=dict(message={'key': "test message"}),
+        check_success: bool=False,
+    ) -> None:
         if available_bots is not None and bots_config is not None:
             server.available_bots = available_bots
             bots_lib_modules = server.load_lib_modules()
