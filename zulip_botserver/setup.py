6151f2db4007d31425fa398c996a61469949fb7f
Eeshan Garg <jerryguitarist@gmail.com>
2018-08-17 22:21:01 -0230

    pypi: Release version 0.5.3.

--- zulip_botserver/setup.py
+++ zulip_botserver/setup.py
@@ -7,7 +7,7 @@ import sys
 if False:
     from typing import Any, Dict, Optional
 
-ZULIP_BOTSERVER_VERSION = "0.5.2"
+ZULIP_BOTSERVER_VERSION = "0.5.3"
 
 # We should be installable with either setuptools or distutils.
 package_info = dict(
