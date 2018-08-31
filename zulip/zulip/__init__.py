048fa79a7c8759a2c414cd0c54d19114fb9e3718
Eeshan Garg <jerryguitarist@gmail.com>
2018-08-31 17:25:55 -0230

    pypi: Release version 0.5.4.
    Starting now, we now also have a CHANGELOG to record major changes
    to the zulip package.

--- zulip/zulip/__init__.py
+++ zulip/zulip/__init__.py
@@ -42,7 +42,7 @@ import logging
 import six
 from typing import Any, Callable, Dict, Iterable, IO, List, Mapping, Optional, Text, Tuple, Union
 
-__version__ = "0.5.3"
+__version__ = "0.5.4"
 
 logger = logging.getLogger(__name__)
 
