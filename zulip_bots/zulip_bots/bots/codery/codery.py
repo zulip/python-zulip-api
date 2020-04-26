e6ee6c382a61b531c221cdee8fe72d35b5defdb3
soumi7 <soumibardhan10@gmail.com>
2020-04-26 23:16:45 +0530

    c6

--- zulip_bots/zulip_bots/bots/codery/codery.py
+++ zulip_bots/zulip_bots/bots/codery/codery.py
@@ -1,11 +1,21 @@
 # See readme.md for instructions on running this code.
 import logging
 from urllib import parse
-import sys, os, zulip
-sys.path.insert(0,os.getcwd())
+import sys
+import os
+import zulip
+sys.path.insert(0, os.getcwd())
 
 import requests
-import calculator,todo, dictionary,news,geekjokes,courses, jobs, leaderboard,trendingproblems
+import calculator
+import todo
+import dictionary
+import news
+import geekjokes
+import courses
+import jobs
+import leaderboard
+import trendingproblems
 from bs4 import BeautifulSoup
 
 from typing import Dict, Any, Union, List
@@ -36,19 +46,17 @@ def get_codery_result(codery_keywords: str) -> str:
                    " * @Codery man execvp\n" \
                    " * @Codery news corona"
 
-    
-
     codery_keywords = codery_keywords.strip()
-    codery_keywords_list=codery_keywords.split(" ")
+    codery_keywords_list = codery_keywords.split(" ")
 
     if codery_keywords == 'help':
         return help_message
 
     elif codery_keywords_list[0] == "todo":
-        return todo.get_todo_response(codery_keywords,CoderyHandler)
+        return todo.get_todo_response(codery_keywords, CoderyHandler)
 
     elif codery_keywords_list[0] == "jobs":
-        return jobs.get_jobs(codery_keywords,CoderyHandler)
+        return jobs.get_jobs(codery_keywords, CoderyHandler)
 
     elif codery_keywords_list[0] == "leaderboard":
         return leaderboard.get_leaderboard()
@@ -57,42 +65,37 @@ def get_codery_result(codery_keywords: str) -> str:
         return trendingproblems.get_problems()
 
     elif codery_keywords_list[0] == "search" or codery_keywords_list[0] == "dictionary":
-        return dictionary.get_dictionary_response(codery_keywords,CoderyHandler)
+        return dictionary.get_dictionary_response(codery_keywords, CoderyHandler)
 
     elif codery_keywords_list[0] == "courses" or codery_keywords_list[0] == "course":
-        return courses.get_courses(codery_keywords,CoderyHandler)
+        return courses.get_courses(codery_keywords, CoderyHandler)
 
     elif codery_keywords_list[0] == "jokes" or codery_keywords_list[0] == "joke":
-        return geekjokes.get_joke(codery_keywords,CoderyHandler)
+        return geekjokes.get_joke(codery_keywords, CoderyHandler)
 
-    elif codery_keywords_list[0]== "calculator":
-        return "The answer is"+calculator.get_calculator_response(codery_keywords,CoderyHandler)
+    elif codery_keywords_list[0] == "calculator":
+        return "The answer is"+calculator.get_calculator_response(codery_keywords, CoderyHandler)
 
     elif codery_keywords_list[0] == "man":
-        
+
         os.system("man " + codery_keywords_list[1] + " > man.txt")
-        file = open("man.txt","r")
+        file = open("man.txt", "r")
         return file.read()
 
     elif codery_keywords_list[0] == "news":
-        return news.get_news_response(codery_keywords,CoderyHandler)
+        return news.get_news_response(codery_keywords, CoderyHandler)
 
-    elif codery_keywords =='contests':
-        
+    elif codery_keywords == 'contests':
 
         URL = 'https://www.stopstalk.com/contests'
-
-
-        
         content = requests.get(URL)
         soup = BeautifulSoup(content.text, 'html.parser')
-        contentTable  = soup.find('table', { "class" : "centered bordered"}) # Use dictionary to pass key : value pair
-
+        contentTable  = soup.find('table', {"class": "centered bordered"})  # Use dictionary to pass key : value pair
 
 
         rows  = contentTable.find_all('tr')
-        l=[]
-        i=0
+        l = []
+        i = 0
 
         for row in rows[1:]:
             l.append("##")
@@ -100,39 +103,30 @@ def get_codery_result(codery_keywords: str) -> str:
             for column in columns:
                 if column.get_text() != "":
                     l.append((column.get_text()).strip() + "@@")
-        
-    
+
             l.append((columns[4].find('a')['href']).strip())
-            i+=1
-            
-        l1="The top contests and hackathons of  today are \n"
+            i += 1
+
+        l1 = "The top contests and hackathons of  today are \n"
         for r in l:
             allContest = r.split("##")
             for eachContest in allContest:
                 attrList = eachContest.split("@@")
                 for attr in attrList:
-                    l1+=attr+"\n"
-
-            
+                    l1 += attr+"\n"
 
         return l1
 
     #return a list of top contests
-    elif codery_keywords =='top contest':
+    elif codery_keywords == 'top contest':
         URL = 'https://www.stopstalk.com/contests'
-
-
-        
         content = requests.get(URL)
         soup = BeautifulSoup(content.text, 'html.parser')
-        contentTable  = soup.find('table', { "class" : "centered bordered"}) # Use dictionary to pass key : value pair
+        contentTable  = soup.find('table', {"class": "centered bordered"})  # Use dictionary to pass key : value pair
 
-
-        
-        
         rows  = contentTable.find_all('tr')
-        l=[]
-        i=0
+        l = []
+        i = 0
 
         for row in rows[1:]:
             l.append("##")
@@ -140,45 +134,36 @@ def get_codery_result(codery_keywords: str) -> str:
             for column in columns:
                 if column.get_text() != "":
                     l.append((column.get_text()).strip() + "@@")
-        
-    
+
             l.append((columns[4].find('a')['href']).strip())
-            i+=1
-            if i==1:
+            i += 1
+            if i == 1:
                 break
-        l1=""
+        l1 = ""
         for r in l:
             allContest = r.split("##")
             for eachContest in allContest:
                 attrList = eachContest.split("@@")
                 for attr in attrList:
-                    l1+=attr+"\n"
-
-            
+                    l1 += attr+"\n"
 
         return l1
-    
-    #to return a list of n top contests
-    elif len(codery_keywords_list)==3:
 
-        if codery_keywords_list[0]=="top" and codery_keywords_list[2]=="contests":
-            n=int(codery_keywords_list[1])
+    #to return a list of n top contests
+    elif len(codery_keywords_list) == 3:
+
+        if codery_keywords_list[0] == "top" and codery_keywords_list[2] == "contests":
+            n = int(codery_keywords_list[1])
         else:
             help_message
         URL = 'https://www.stopstalk.com/contests'
-
-
-        
         content = requests.get(URL)
         soup = BeautifulSoup(content.text, 'html.parser')
-        contentTable  = soup.find('table', { "class" : "centered bordered"}) # Use dictionary to pass key : value pair
+        contentTable  = soup.find('table', {"class": "centered bordered"})  # Use dictionary to pass key : value pair
 
-
-        
-        
         rows  = contentTable.find_all('tr')
-        l=[]
-        i=0
+        l = []
+        i = 0
 
         for row in rows[1:]:
             l.append("##")
@@ -186,33 +171,24 @@ def get_codery_result(codery_keywords: str) -> str:
             for column in columns:
                 if column.get_text() != "":
                     l.append((column.get_text()).strip() + "@@")
-        
-    
+
             l.append((columns[4].find('a')['href']).strip())
-            i+=1
-            if i==n:
+            i += 1
+            if i == n:
                 break
-        l1=""
+        l1 = ""
         for r in l:
             allContest = r.split("##")
             for eachContest in allContest:
                 attrList = eachContest.split("@@")
                 for attr in attrList:
-                    l1+=attr+"\n"
-
-            
+                    l1 += attr+"\n"
 
         return l1
-
-        
 
     elif codery_keywords == '' or codery_keywords is None:
         return help_message
 
-
-
-        
-    
 class CoderyHandler(object):
     '''
     This plugin allows users to get the competitive
