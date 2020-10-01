9e26d35d2c228db2e665c112984e318bfa426299
QEDK <1994constant@gmail.com>
2020-10-02 01:50:11 +0530

    trac: Log invalid API credentials

--- zulip/integrations/trac/zulip_trac.py
+++ zulip/integrations/trac/zulip_trac.py
@@ -21,6 +21,10 @@ VERSION = "0.9"
 
 from typing import Any, Dict
 
+import logging
+logging.basicConfig()
+log = logging.getLogger("zulip-trac-post")
+
 if config.ZULIP_API_PATH is not None:
     sys.path.append(config.ZULIP_API_PATH)
 
@@ -46,12 +50,15 @@ def trac_subject(ticket: Any) -> str:
     return truncate("#%s: %s" % (ticket.id, ticket.values.get("summary")), 60)
 
 def send_update(ticket: Any, content: str) -> None:
-    client.send_message({
-        "type": "stream",
-        "to": config.STREAM_FOR_NOTIFICATIONS,
-        "content": content,
-        "subject": trac_subject(ticket)
-    })
+    if client.get_profile()["result"] == "error":
+        log.error(zulip.InvalidCredentialsError("Invalid API credentials"))
+    else:
+        client.send_message({
+            "type": "stream",
+            "to": config.STREAM_FOR_NOTIFICATIONS,
+            "content": content,
+            "subject": trac_subject(ticket)
+        })
 
 class ZulipPlugin(Component):
     implements(ITicketChangeListener)
