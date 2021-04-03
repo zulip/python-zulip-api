42aecf683e4e7628f19796b440b0be4ed96a406d
PIG208 <359101898@qq.com>
2021-04-04 06:02:12 +0800

    zulip-bots: Fix incrementor to handle edit time limit error.
    When the incrementor attempts to edit a message that was sent long
    ago, it will fail and the message will not be updated, nor will a
    new message be sent.
    
    Fixes: #673

--- zulip_bots/zulip_bots/bots/incrementor/test_incrementor.py
+++ zulip_bots/zulip_bots/bots/incrementor/test_incrementor.py
@@ -29,3 +29,27 @@ class TestIncrementorBot(BotTestCase, DefaultTests):
             for item in m.call_args_list
         ]
         self.assertEqual(content_updates, ['2', '3', '4'])
+
+    def test_bot_edit_timeout(self) -> None:
+        bot = get_bot_message_handler(self.bot_name)
+        bot_handler = StubBotHandler()
+
+        message = dict(type='stream')
+
+        bot.initialize(bot_handler)
+        bot.handle_message(message, bot_handler)
+
+        error_msg = dict(msg='The time limit for editing this message has passed', result='error')
+        with patch('zulip_bots.test_lib.StubBotHandler.update_message', return_value=error_msg):
+            with patch('zulip_bots.simple_lib.SimpleMessageServer.send') as m:
+                bot.handle_message(message, bot_handler)
+                bot.handle_message(message, bot_handler)
+
+        # When there is an error, the bot should resend the message with the new value.
+        self.assertEqual(m.call_count, 2)
+
+        content_updates = [
+            item[0][0]['content']
+            for item in m.call_args_list
+        ]
+        self.assertEqual(content_updates, ['2', '3'])
