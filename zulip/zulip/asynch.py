897635616d7b6bcb940cc2f0950d30f9c17f10d8
Alex Dehnert <adehnert@mit.edu>
2021-11-29 00:22:31 -0500

    asynch: fix backoff lint

--- zulip/zulip/asynch.py
+++ zulip/zulip/asynch.py
@@ -1,5 +1,7 @@
 import asyncio
 import json
+import logging
+import random
 import sys
 import traceback
 import urllib.parse
@@ -20,6 +22,8 @@ from typing import (
 import aiohttp
 
 import zulip
+
+logger = logging.getLogger(__name__)
 
 API_VERSTRING = "v1/"
 
